e1ed18787b643febb9af96a95a823b474e1afdb9
严俊东 <1172482914@qq.com>
2021-04-16 16:08:17 +0800

    绘制中心点和箭头

--- miniprogram/js/runtime/player.js
+++ miniprogram/js/runtime/player.js
@@ -1,9 +1,12 @@
+
+const DIR_MARGIN = 20 // 箭头离中心点距离，值越大箭头离中心点越远
+const DIR_LENGTH = 5 // 方向箭头长度（单边），值越大箭头越大
 export default class Player {
   constructor() {
 
   }
 
-  render(ctx) {
+  render(ctx, angle = 90) {
     // 绘制一个中心点
     const width = window.innerWidth
     const height = window.innerHeight
@@ -12,12 +15,18 @@ export default class Player {
     ctx.fillStyle = '#b0efeb'
     ctx.fill()
 
+    // 绘制方向箭头
+    const r = Math.sqrt(Math.pow(DIR_LENGTH, 2) * 2) / 2
+    const sinAngle = Math.sin(2 * Math.PI / 360 * angle)
+    const cosAngle = Math.cos(2 * Math.PI / 360 * angle)
+    const centerPoint = {
+      x: width / 2 + DIR_MARGIN * cosAngle,
+      y: height / 2 - DIR_MARGIN * sinAngle
+    }
     ctx.beginPath()
-    const r = Math.sqrt(Math.pow(5, 2) * 2) / 2
-    const margin = 20
-    ctx.moveTo(width / 2 - r, height / 2 - margin)
-    ctx.lineTo(width / 2, height / 2 - margin - r)
-    ctx.lineTo(width / 2 + r, height / 2 - margin)
+    ctx.moveTo(centerPoint.x - sinAngle * r, centerPoint.y - cosAngle * r )
+    ctx.lineTo(centerPoint.x + cosAngle * r, centerPoint.y - sinAngle * r )
+    ctx.lineTo(centerPoint.x + sinAngle * r, centerPoint.y + cosAngle * r )
     ctx.strokeStyle = '#eca3f5'
     ctx.stroke()
   }
