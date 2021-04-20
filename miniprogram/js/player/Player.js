daa1c1e0cbdb6951097244437a4d474f55284c25
严俊东 <1172482914@qq.com>
2021-04-20 15:27:20 +0800

    蛇神从尾部开始绘制

--- miniprogram/js/player/Player.js
+++ miniprogram/js/player/Player.js
@@ -16,16 +16,18 @@ export default class Player {
     // 绘制一个中心点
     let headerMoveX
     let headerMoveY
-    for (let i = dataBus.snakePoints.length - 1; i >= 0; i--) {
+    const snakeLength = dataBus.snakePoints.length
+
+    for (let i = snakeLength - 1; i >= 0; i--) {
       const point = dataBus.snakePoints[i]
       const prePoint = dataBus.snakePoints[i + 1]
-      if (i === dataBus.snakePoints.length - 1) {
+      if (i === snakeLength - 1) {
         // 头部，正常绘制
         // 下一帧移动距离
         headerMoveY = Math.sin(2 * Math.PI / 360 * angle) * dataBus.mapSpeed
         headerMoveX = Math.cos(2 * Math.PI / 360 * angle) * dataBus.mapSpeed
         point.gs.unshift(angle)
-        point.render(ctx)
+        // point.render(ctx)
       } else {
         // 身体部分
         // 获取上个原点历史第五（圆半径*2/速度）帧移动的角度
@@ -35,13 +37,14 @@ export default class Player {
         point.gs.unshift(historyAngle)
         point.x = point.x + x - headerMoveX
         point.y = point.y - y + headerMoveY
-        point.render(ctx)
+        // point.render(ctx)
         if (i === 0) {
           point.gs.pop()
         }
       }
     }
-    
+    // 从尾部开始绘制
+    dataBus.snakePoints.forEach(point => point.render(ctx))
   }
   renderPoint() {
 
